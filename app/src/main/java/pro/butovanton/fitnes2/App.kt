238c4008fe7dcf5819fa3cfbb0772cade0f95e76
AntonButov <butov6101@gmail.com>
2020-10-14 18:41:55 +0300

    Подключил multidex.

--- app/src/main/java/pro/butovanton/fitnes2/App.kt
+++ app/src/main/java/pro/butovanton/fitnes2/App.kt
@@ -1,2 +1,14 @@
 package pro.butovanton.fitnes2
 
+import android.app.Application
+import com.htsmart.wristband2.WristbandApplication
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        WristbandApplication.init(this);
+        WristbandApplication.setDebugEnable(true);
+    }
+
+}
